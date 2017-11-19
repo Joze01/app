ee3fa101fd483e0766210f7d8f009f6990f7bc38
Jonathan Palma <tanpalma04@gmail.com>
2017-11-19 07:57:49 -0800

    ALV :c

--- www/app/components/propuestaDetalle/detalle-controller.js
+++ www/app/components/propuestaDetalle/detalle-controller.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module("ngapp").controller("detallecontroller", function(shared, $state, $scope, $mdSidenav, $mdComponentRegistry){
+angular.module("ngapp").controller("detallecontroller", function (shared, $state, $scope, $http, $mdSidenav, $mdComponentRegistry, $sessionStorage) {
 
     var ctrl = this;
 
@@ -10,24 +10,32 @@ angular.module("ngapp").controller("detallecontroller", function(shared, $state,
 
     this.title = $state.current.title;
 
-
-    this.isOpen = function() { return false };
+    this.isOpen = function () { return false };
     $mdComponentRegistry
-    .when("left")
-    .then( function(sideNav){
-      ctrl.isOpen = angular.bind( sideNav, sideNav.isOpen );
-      ctrl.toggle = angular.bind( sideNav, sideNav.toggle );
-    });
-
-    this.toggleRight = function() {
-    $mdSidenav("left").toggle()
-        .then(function(){
+        .when("left")
+        .then(function (sideNav) {
+            ctrl.isOpen = angular.bind(sideNav, sideNav.isOpen);
+            ctrl.toggle = angular.bind(sideNav, sideNav.toggle);
         });
+
+    this.toggleRight = function () {
+        $mdSidenav("left").toggle()
+            .then(function () {
+            });
     };
 
-    this.close = function() {
-    $mdSidenav("right").close()
-        .then(function(){
-        });
+    this.close = function () {
+        $mdSidenav("right").close()
+            .then(function () {
+            });
     };
+
+    if ($sessionStorage.proposalId != null) {
+        // $http.get("http://192.168.43.233/opinapp/api/propuestas/" + $sessionStorage.proposalId)
+        $http.get("/detalle.json")
+            .success(function (data) {
+                $scope.proposal = data[0];
+                console.log($scope.proposal);
+            });
+    }
 });
